7be855dfb2121e503adacaad258e29c42681ba19
Emanuel Birge <emanuel.birge@gmail.com>
2013-03-08 12:26:43 +0100

    modified example script

--- example.py
+++ example.py
@@ -1,10 +1,31 @@
-import pyrtaudio as pyrt
+import pyrtaudio as p
+import wave
 
 
-p = pyrt.RtAudio()
-print dir(p)
+f = wave.open('somefile.wav')
 
-d=p.getDeviceInfo()
-print d
+def formatfromwidth(sampwidth):
+    if sampwidth == 1:
+        return p.RTAUDIO_SINT8
+    if sampwidth == 2:
+        return p.RTAUDIO_SINT16
+    if sampwidth == 4:
+        return p.RTAUDIO_SINT32
+    #etcetera
 
-#playback / recording not yet supported :)
+def callback():
+    frames = f.readframes(512)
+    if frames == '':
+        return None
+    return frames
+
+r=p.RtAudio()
+r.openStream( {'device_id':0, 'channels':f.getnchannels(), 'first_channel':0},
+        None, #only playback support yet
+        formatfromwidth(f.getsampwidth()),
+        f.getframerate(),
+        512, #buffer size in frames
+        callback)
+r.startStream() #wooh!
+
+
