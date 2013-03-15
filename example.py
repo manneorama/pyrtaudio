5803ca83d867ffaa246347014dfef950a3cd0772
Emanuel Birge <emanuel.birge@gmail.com>
2013-03-15 11:19:17 +0100

    updated example.py. pyrtaudio now supports rendering OR capturing of audio. duplex mode not implemented yet

--- example.py
+++ example.py
@@ -1,31 +1,60 @@
 import pyrtaudio as p
-import wave
+import wave, time
 
+## simple playback (rendering) example
+fr = wave.open('somefile.wav', 'r')
+bframes = 512
 
-f = wave.open('somefile.wav')
-
-def formatfromwidth(sampwidth):
-    if sampwidth == 1:
-        return p.RTAUDIO_SINT8
-    if sampwidth == 2:
-        return p.RTAUDIO_SINT16
-    if sampwidth == 4:
-        return p.RTAUDIO_SINT32
-    #etcetera
-
-def callback():
-    frames = f.readframes(512)
+def render_callback():
+    frames = fr.readframes(bframes)
     if frames == '':
-        return None
+        return None #stops the stream
     return frames
 
+def format_from_width(w):
+    if w == 1:
+        return p.RTAUDIO_SINT8
+    if w == 2:
+        return p.RTAUDIO_SINT16
+    if w == 4:
+        return p.RTAUDIO_SINT32
+
 r=p.RtAudio()
-r.openStream( {'device_id':0, 'channels':f.getnchannels(), 'first_channel':0},
-        None, #only playback support yet
-        formatfromwidth(f.getsampwidth()),
-        f.getframerate(),
-        512, #buffer size in frames
-        callback)
-r.startStream() #wooh!
+r.open_stream( {'device_id':0, 'channels':fr.getnchannels(), 'first_channel':0},
+        None, #no input parameters -> only playback
+        format_from_width(fr.getsampwidth()),
+        fr.getframerate(),
+        bframes, #buffer size in frames
+        render_callback)
 
+r.start_stream() #wooh!
+time.sleep(2)
+r.close_stream()
 
+fr.close()
+
+## simple recording (capture) example
+channels = 1
+rate = 44100
+width = 2
+fw = wave.open('someotherfile.wav', 'w')
+fw.setframerate(rate)
+fw.setsampwidth(width)
+fw.setnchannels(channels)
+
+def capture_callback(frames):
+    fw.writeframes(frames)
+    return 0 #returning None will stop the stream
+
+r.open_stream(None,
+    {'device_id':0, 'channels':channels, 'first_channel':0},
+    format_from_width(width),
+    rate,
+    bframes,
+    capture_callback)
+
+r.start_stream()
+time.sleep(4)
+r.close_stream()
+
+fw.close()
